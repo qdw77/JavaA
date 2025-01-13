520ec7383c11cc3103080f8566c3a905eb0f675d
301-10 <301-10@DESKTOP-GES21Q6>
2025-01-13 17:49:27 +0900

    <20250113> 309p 예제작성 및 자바/파이썬 14일 코딩 테스트

---
+++ thisisjava/src/ch07/sec05/exam02/Car.java
@@ -0,0 +1,22 @@
+package ch07.sec05.exam02;
+
+public class Car {
+	/*
+	 * protected(자식한테만 공개) 여야함
+	 * 303p~
+	 * 
+	 * 타입변환 306p
+	 * ex)
+	 * B extends A
+	 * 
+	 * A a = new A()
+	 * B b=new B()
+	 * 
+	 * 타입변환
+	 * A a = new B();
+	 * 308p 비교연산자
+	 * 밑 관계도 > 맨위 a,b,c,d,e 트리구조 자식 세개에 대한 구조 bc 2세대/de 3세대 
+	 * > 상속 계층에서 상위타입이라면 자동 타입 변환이 일어날 수 있다.
+	 * 
+	 * */
+}
---
+++ thisisjava/src/ch07/sec07/exam01/A.java
@@ -0,0 +1,34 @@
+package ch07.sec07.exam01;
+
+class A {
+
+}
+
+class B extends A{
+	
+}
+
+class C extends A{
+	
+}
+
+class D extends B{
+	
+}
+
+class E extends C{
+	
+}
+
+public class PromotionExample() {
+	public static void main(String[] args) {
+		B b = new B();
+		C c = new C();
+		D d = new D();
+		E e = new E();
+		
+		A a1 = b;
+		A a2 =c;
+		A a3 =
+	}
+}
